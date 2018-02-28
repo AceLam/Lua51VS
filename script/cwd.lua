e1256502e7b89fa3277f35486ae8c5f62505c521
AceLam <AceLam@foxmail.com>
2018-02-28 23:53:03 +0800

    require modules

---
+++ script/cwd.lua
@@ -0,0 +1,3 @@
+-- 创建文件以检验 . 的绝对路径是什么
+local f = io.open("./lua_path.tmp", "w");
+f:close();
--- script/main.lua
+++ script/main.lua
@@ -1,1 +1,35 @@
-print("hello lua")
+local info = debug.getinfo(1, "S");
+local path = info.short_src;
+local dir = string.match(path, "^.*[/\\]");
+
+if dir then
+    package.path = package.path .. ";" .. dir .. "?.lua";
+end
+
+
+modules = {
+    --"test.bisect",          --bisection method for solving non-linear equations
+    --"test.cf",              --temperature conversion table (celsius to farenheit)
+    --"test.echo",            --echo command line arguments
+    --"test.env",             --environment variables as automatic global variables
+    --"test.factorial",       --factorial without recursion
+    --"test.fib",             --fibonacci function with cache
+    --"test.fibfor",          --fibonacci numbers with coroutines and generators
+    --"test.globals",         --report global variable usage
+    --"test.hello",           --the first program in every languag
+    --"test.life",            --Conway's Game of Life
+    --"test.luac",            --bare-bones luac
+    "test.printf",          --an implementation of printf
+    --"test.readonly",        --make global variables readonly
+    --"test.sieve",           --the sieve of of Eratosthenes programmed with coroutines
+    --"test.sort",            --two implementations of a sort function
+    --"test.table",           --make table, grouping all data for the same item
+    --"test.trace-calls",     --trace calls
+    --"test.trace-globals",   --trace assigments to global variables
+    --"test.xd",              --hex dump
+    --"cwd",                  --an implementation of printf
+}
+
+for i, v in ipairs(modules) do
+    require(v);
+end
